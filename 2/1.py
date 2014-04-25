b522f2c1e488ffab6d56d9cdb4b742789bd6f3d3
Liam � S�illeabh�in <osuilleabhainliam@gmail.com>
2014-04-25 16:00:32 +0100

    lol

--- 2/1.py
+++ 2/1.py
@@ -5,10 +5,10 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from numpy import *
 
-dt = 0.0001
-dx = 1/100.
+dt = 1/800.
+dx = 1/200.
 dy = dx
-Time = 0.003
+Time = 0.1
 
 X = arange(-1,1,dx)
 Y = arange(-1,1,dy)
@@ -30,7 +30,7 @@ ax.plot_surface(lX,lY,U[0],cmap=cm.coolwarm)
 title("t = 0")
 xlabel("x")
 ylabel("y")
-show(block=False)
+show()
 
 # Equivalent to \alpha_x/y in the notes.
 cx = dt/(dx**2)
@@ -40,42 +40,52 @@ cy2 = cy/2.
 
 Aux = zeros(nx)
 for i in range(0,nx-1):
-    Aux[i] = -cx
+    Aux[i] = -cx2
 Adx = zeros(nx)
 for i in range(0,nx):
-    Adx[i] = 2 + 2*cx #; Adx[0] = 1.; Adx[nx-1] = 1.
+    Adx[i] = 1 + 1*cx ; Adx[0] = 1.; Adx[nx-1] = 1.
 Alx = zeros(nx)
 for i in range(1,nx):
-    Alx[i] = -cx
+    Alx[i] = -cx2
 Auy = zeros(ny)
 for i in range(0,ny-1):
-    Auy[i] = -cy
+    Auy[i] = -cy2
 Ady = zeros(ny)
 for i in range(0,ny):
-    Ady[i] = 2 + 2*cy #; Ady[0] = 1.; Ady[ny-1] = 1.
+    Ady[i] = 1 + 1*cy ; Ady[0] = 1.; Ady[ny-1] = 1.
 Aly = zeros(ny)
 for i in range(1,ny):
-    Aly[i] = -cy
+    Aly[i] = -cy2
 
 Dx = zeros((nx,ny))
 Dy = zeros((nx,ny))
 
 def Tsolve(a, b, c, d):
-    ntmp = len(d) # n is the numbers of rows, a and c has length n-1
-    for i in range(0,ntmp-1):
-        d[i+1] -= d[i] * a[i] / b[i]
-        b[i+1] -= c[i] * a[i] / b[i]
-    for i in reversed(range(0,n-1)):
-        d[i] -= d[i+1] * c[i] / b[i+1]
-    return array([d[i]/b[i] for i in range(0,ntmp)])
+    ntmp = len(d)
+    a[0] = 0 # Make sure.
+    c[ntmp-1] = 0 # Make sure here, too.
+    e = zeros(ntmp)
+    f = e.copy()
+    ret = e.copy()
+    e[0] = c[0]/b[0]
+    f[0] = d[0]/b[0]
+    for i in range(1,ntmp):
+        e[i] = c[i]/(b[i] - a[i]*e[i-1])
+        f[i] = (d[i] - a[i]*f[i-1])/(b[i] - a[i]*e[i-1])
+    ret[ntmp-1] = f[ntmp-1]
+    for i in reversed(range(0,ntmp-1)):
+        ret[i] = f[i] - e[i]*ret[i+1]
+    return ret
+
 
 for n in range(1,nt):
     print("n = " + str(n))
-    for i in range(0,nx):
+    for i in range(1,nx-1):
         for j in range(1,ny-1):
             Dy[i][j] = U[n-1][i][j] + cy2*U[n-1][i][j-1] - cy*U[n-1][i][j] + cy2*U[n-1][i][j+1]
-        TMP = Tsolve(Aly.copy(), Ady.copy(), Auy.copy(), Dy[i].copy())
-        for k in range(0,len(TMP)):
+        TMP = Tsolve(Aly, Ady, Auy, Dy[i])
+#        print(max(TMP))
+        for k in range(1,len(TMP-1)):
             Uhalf[i][k] = TMP[k]
 
     for k in range(0,ny):
@@ -84,10 +94,11 @@ for n in range(1,nt):
         Uhalf[k][ny-1] = 0; Uhalf[k][0] = 0
 
     for i in range(1,nx-1):
-        for j in range(0,ny):
+        for j in range(1,ny-1):
             Dx[i][j] = Uhalf[i][j] + cx2*Uhalf[i-1][j] - cy*Uhalf[i][j] + cx2*Uhalf[i+1][j]
-        TMP = Tsolve(Alx.copy(), Adx.copy(), Aux.copy(), Dx[i].copy())
-        for k in range(0,len(TMP)):
+        TMP = Tsolve(Alx, Adx, Aux, Dx[i])
+#        print(max(TMP))
+        for k in range(1,len(TMP-1)):
             U[n][i][k] = TMP[k]
 
     for k in range(0,ny):
@@ -100,7 +111,7 @@ for n in range(1,nt):
     ax.plot_surface(lX,lY,U[n],cmap=cm.coolwarm)
     xlabel("x")
     ylabel("y")
-    show(block=False)
+    show()
 
 
 # vim: ts=4
